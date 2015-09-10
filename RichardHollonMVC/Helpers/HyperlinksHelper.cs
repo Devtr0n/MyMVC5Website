bd91ac7be76548708231052bb832e30561c84594
Devtr0n <richardsmailbox@gmail.com>
2015-09-10 15:17:00 -0500

    misc updates and fixes, for JSON and media page, etc

--- RichardHollonMVC/Helpers/HyperlinksHelper.cs
+++ RichardHollonMVC/Helpers/HyperlinksHelper.cs
@@ -1,6 +1,6 @@
-﻿using System.Linq;
-using System.Xml.Linq;
+﻿using System.Collections.Generic;
 using System.Web;
+using System.IO;
 
 namespace RichardHollonMVC.Helpers
 {
@@ -8,51 +8,22 @@ namespace RichardHollonMVC.Helpers
     {
         public class FavoriteHyperlink
         {
-            private readonly string title;
-            private readonly string url;
-
-            public string Title { get { return this.title; } }
-            public string URL { get { return this.url; } }
-
-            public FavoriteHyperlink(string title, string url)
-            {
-                this.title = title;
-                this.url = url;
-            }
+            public List<FavoriteHyperlink> items { get; set; }
+            public string Title { get; set; }
+            public string URL { get; set; }
         }
-
-        public static FavoriteHyperlink[] GetFavoriteHyperlinks(int count)
-        {
-            string path = HttpContext.Current.Server.MapPath("~/Content/hyperlinks.xml");
-            var rows = XDocument.Load(path)
-                                .Root.Elements()
-                                .Select(row => new FavoriteHyperlink(row.Element("title").Value, row.Element("url").Value));
-
-            return rows.Take(count).ToArray();
-
-        }
-
+        
         public static FavoriteHyperlink[] GetRandomizedHyperlinks()
         {
-            var rndHyperlinks = RandomizerHelper.Shuffle(HyperlinksHelper.GetFavoriteHyperlinks(6));
-            return rndHyperlinks;
+            string path = HttpContext.Current.Server.MapPath("~/Content/Hyperlinks.json");
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                var links = (FavoriteHyperlink)Newtonsoft.Json.JsonConvert.DeserializeObject(json, typeof(FavoriteHyperlink));
+
+                return links.items.ToArray();
+            }           
+
         }
-
-        //public static StringBuilder GetRandomizedHyperlinks()
-        //{
-        //    var rndHyperlinks = RandomizerHelper.Shuffle(HyperlinksHelper.GetFavoriteHyperlinks(6));
-
-        //    StringBuilder hyperlinks = new StringBuilder();
-        //    hyperlinks.Append("<div id='randomHyperlinks'><p><ul>");
-
-        //    foreach (var v in rndHyperlinks)
-        //    {
-        //        hyperlinks.Append(string.Format("<li><a href={0} target={1}>{2}</a></li>", v.URL, "_blank", v.Title));
-        //    }
-
-        //    hyperlinks.Append("</ul></p></div>");
-
-        //    return hyperlinks;
-        //}
     }
 }
